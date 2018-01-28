ec0f7e746e8922238d14ac65e382dc058fe5e264
Tim Born <timborn@deltacfax.com>
2018-01-28 13:01:09 -0600

    fix wget args for intellij install

--- src/common/install/intellij.sh
+++ src/common/install/intellij.sh
@@ -5,7 +5,7 @@ set -e
 echo "Install IntelliJ Ultimate"
 
 mkdir -p $INTELLIJ_HOME
-wget -qO- https://download.jetbrains.com/idea/ideaIU-2017.3.3.tar.gz | tar xz -
+wget -qO- https://download.jetbrains.com/idea/ideaIU-2017.3.3.tar.gz | tar xz --strip 1 -C $INTELLIJ_HOME
 
 # TODO: INSTALL IS INTERACTIVE!  FIX THIS to be unattended
 # $INTELLIJ_HOME/bin/idea.sh	# install
