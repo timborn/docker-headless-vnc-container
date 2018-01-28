a836552ba5b62d5b28da4b139e9b9529ed83a14e
Tim Born <timborn@deltacfax.com>
2018-01-28 14:10:39 -0600

    intellij installs without being interactive

--- src/common/install/intellij.sh
+++ src/common/install/intellij.sh
@@ -7,6 +7,8 @@ echo "Install IntelliJ Ultimate"
 mkdir -p $INTELLIJ_HOME
 wget -qO- https://download.jetbrains.com/idea/ideaIU-2017.3.3.tar.gz | tar xz --strip 1 -C $INTELLIJ_HOME
 
-# TODO: INSTALL IS INTERACTIVE!  FIX THIS to be unattended
-# $INTELLIJ_HOME/bin/idea.sh	# install
+# this gets created as part of the interactive stuff; maybe skip interactive?
+tar xvzf $INST_SCRIPTS/intellij-setup.tgz -C $HOME
+
 echo PATH=\$PATH:$INTELLIJ_HOME/bin >> $HOME/.bashrc
+echo "alias intellij=/headless/intellij/bin/idea.sh" >> $HOME/.bashrc
